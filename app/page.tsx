f8edf63575364fc37e49671634d596cf3863ab46
Samuel Portela <99668808+Samuel-3004@users.noreply.github.com>
2025-12-17 13:09:40 -0300

    feat: add initial home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,56 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import BarbershopItem from "@/components/barbershop-item";
+import BookingItem from "@/components/booking-item";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import {
+  PageContainer,
+  PageSectionContent,
+  PageSectionScroller,
+  PageSectionTitle,
+} from "@/components/ui/page";
+import { getBarbershops, getPopularBarbershops } from "@/data/barbershops";
+import banner from "@/public/banner.png";
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const barbershops = await getBarbershops();
+  const popularBarbershops = await getPopularBarbershops();
   return (
-    <>
-      <div className="flex h-screen flex-col items-center justify-center">
-        <Button variant="destructive">Clique Aqui</Button>{" "}
-        <Input type="text" placeholder="Enter your name" />
-      </div>
-    </>
+    <div>
+      <Header />
+      <PageContainer>
+        <Image
+          src={banner}
+          alt="Agende nos melhores com a Aparatus"
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+
+        <PageSectionContent>
+          {/* Composition Pattern */}
+          <PageSectionTitle>Agendamentos</PageSectionTitle>
+          <BookingItem />
+        </PageSectionContent>
+
+        <PageSectionContent>
+          <PageSectionTitle>Barbearias</PageSectionTitle>
+          <PageSectionScroller>
+            {barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+          </PageSectionScroller>
+        </PageSectionContent>
+
+        <PageSectionContent>
+          <PageSectionTitle>Populares</PageSectionTitle>
+          <PageSectionScroller>
+            {popularBarbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+          </PageSectionScroller>
+        </PageSectionContent>
+      </PageContainer>
+      {/* <Footer /> */}
+    </div>
   );
 }
---
+++ components/header.tsx
@@ -0,0 +1,15 @@
+import { MenuIcon } from "lucide-react";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="bg-background flex items-center justify-between px-5 py-6">
+      <Image src="/logo.svg" alt="Logo Aparatus" width={91} height={24} />
+      <Button variant="outline" size="icon">
+        <MenuIcon />
+      </Button>
+    </header>
+  );
+};
+export default Header;
